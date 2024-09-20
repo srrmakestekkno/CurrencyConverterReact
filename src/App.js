66f3c8a2c38241651e5cec0cadafc2a38f7e5893
stian røgeberg <srr007@hotmail.com>
2024-09-20 10:19:42 +0200

    First commit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,85 @@
-import logo from './logo.svg';
-import './App.css';
+import { useEffect, useState } from "react";
 
 function App() {
+  const [amount, setAmount] = useState(1);
+  const [fromCurrency, setFromCurrency] = useState("EUR");
+  const [toCurrency, setToCurrency] = useState("USD");
+  const [result, setResult] = useState("");
+  const [isLoading, setIsLoading] = useState(false);
+
+  useEffect(() => {
+    const controller = new AbortController();
+
+    const convert = async () => {
+      try {
+        setIsLoading(true);
+        const res = await fetch(
+          `https://api.frankfurter.app/latest?amount=${amount}&from=${fromCurrency}&to=${toCurrency}`,
+          { signal: controller.length }
+        );
+
+        if (!res.ok) {
+          throw new Error("Something went wrong...");
+        }
+
+        const data = await res.json();
+
+        if (data.Response === "False") {
+          throw new Error("No response...");
+        }
+
+        setResult(data.rates[toCurrency]);
+        setIsLoading(false);
+      } catch (err) {
+        console.log(err.message);
+      }
+    };
+
+    if (fromCurrency === toCurrency) return setResult(amount);
+
+    convert();
+
+    return () => {
+      controller.abort();
+    };
+  }, [amount, toCurrency, fromCurrency]);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+    <div>
+      <input
+        value={amount}
+        type="text"
+        onChange={(e) => setAmount(Number(e.target.value))}
+        disabled={isLoading}
+      />
+      <label>From: </label>
+      <select
+        value={fromCurrency}
+        onChange={(e) => setFromCurrency(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      <label>To: </label>
+      <select
+        value={toCurrency}
+        onChange={(e) => setToCurrency(e.target.value)}
+        disabled={isLoading}
+      >
+        <option value="USD">USD</option>
+        <option value="EUR">EUR</option>
+        <option value="CAD">CAD</option>
+        <option value="INR">INR</option>
+      </select>
+      {isLoading && <p>Please wait while converting...</p>}
+      {result && !isLoading && (
         <p>
-          Edit <code>src/App.js</code> and save to reload.
+          {result} {toCurrency}
         </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      )}
     </div>
   );
 }
